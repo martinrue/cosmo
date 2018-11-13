8664ad81a3bb5657cdd14a7efe12acb6f8fc2845
Martin Rue <hello@martinrue.com>
2018-11-13 20:22:08 +0000

    Add condition in tests to check for precise error

--- commands/steps_test.go
+++ commands/steps_test.go
@@ -13,10 +13,11 @@ func TestCommandStepsErrors(t *testing.T) {
 		Name     string
 		Args     []string
 		Expected string
+		Err      error
 	}{
-		{"invalid flag", []string{"task", "--invalid"}, "Usage: cosmo steps <task> [--server=<name>]\n"},
-		{"missing task", []string{}, "Usage: cosmo steps <task> [--server=<name>]\n"},
-		{"ambiguous task", []string{"task-1"}, "task 'task-1' is ambiguous, specify server\n"},
+		{"invalid flag", []string{"task", "--invalid"}, "Usage: cosmo steps <task> [--server=<name>]\n", commands.ErrFlagParse},
+		{"missing task", []string{}, "Usage: cosmo steps <task> [--server=<name>]\n", commands.ErrNoTask},
+		{"ambiguous task", []string{"task-1"}, "task 'task-1' is ambiguous, specify server\n", commands.ErrFindTask},
 	}
 
 	for _, test := range tests {
@@ -27,8 +28,14 @@ func TestCommandStepsErrors(t *testing.T) {
 			}
 
 			buffer := &bytes.Buffer{}
-			if _, err := commands.NewCommandSteps(cfg, test.Args, buffer); err == nil {
+			_, err = commands.NewCommandSteps(cfg, test.Args, buffer)
+
+			if err == nil {
 				t.Fatalf("expected ctor to return err")
+			}
+
+			if err != test.Err {
+				t.Fatalf("expected ctor to return err (%v), got (%v)", test.Err, err)
 			}
 
 			if buffer.String() != test.Expected {
@@ -37,3 +44,47 @@ func TestCommandStepsErrors(t *testing.T) {
 		})
 	}
 }
+
+// func TestCommandStepsOutput(t *testing.T) {
+// 	stringMatchesGoldenFile := func(t *testing.T, str string, filename string) bool {
+// 		golden, err := ioutil.ReadFile(path.Join("testdata", "steps", filename))
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+
+// 		return str == string(golden)
+// 	}
+
+// 	tests := []struct {
+// 		Name       string
+// 		Args       []string
+// 		GoldenFile string
+// 	}{
+// 		{"all servers", []string{}, "all-servers.golden"},
+// 		// {"server 1", []string{"--server", "server-1"}, "server-1.golden"},
+// 		// {"server 2", []string{"--server", "server-2"}, "server-2.golden"},
+// 	}
+
+// 	for _, test := range tests {
+// 		t.Run(test.Name, func(t *testing.T) {
+// 			cfg, err := config.Read("testdata/steps/steps.toml")
+// 			if err != nil {
+// 				t.Fatalf("expected config read err to be nil, got (%v)", err)
+// 			}
+
+// 			buffer := &bytes.Buffer{}
+// 			task, err := commands.NewCommandSteps(cfg, test.Args, buffer)
+// 			if err != nil {
+// 				t.Fatalf("expected ctor to return nil err, got (%v)", err)
+// 			}
+
+// 			if err := task.Exec(); err != nil {
+// 				t.Fatalf("expected exec to return nil err, got (%v)", err)
+// 			}
+
+// 			if !stringMatchesGoldenFile(t, buffer.String(), test.GoldenFile) {
+// 				t.Fatalf("command output does not match golden file")
+// 			}
+// 		})
+// 	}
+// }
